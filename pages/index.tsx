8c50592bfd0107f16fc4918e1998709294ffa7c4
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-11-12 21:30:30 +0530

    landing page init :lantern:

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,48 +11,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="flex flex-col w-full h-full px-10 pt-20">
+        <div className="flex flex-col w-1/2 h-full items-center justify-center gap-20">
+          <h1 className="text-7xl">
+            All About <span className="text-blue-500">Programming</span>
+          </h1>
+          <p className="text-xl w-full h-full">
+            Lorem ipsum dolor sit amet consectetur. Aenean donec vitae euismod tristique iaculis nisl lorem. Sed id dignissim gravida elit. In et habitant scelerisque enim mattis velit nibh at. Viverra id kjdhsfkihafidhawofkihdsifhaesfghighdusghdrhsgdhsgihdgkihdf
+            kjdhfidshfidhsfidhshf lekhfdshfdshfdksh idhfidhfidhsf idhfidhfdfihi uidhfihd
+          </p>
+          <div className="w-full h-full flex flex-col sm:flex-row items-center justify-evenly text-black text-xl">
+            <div className="flex w-1/3 rounded-full bg-gray-400 p-4 items-center justify-center">Register</div>
+            <div className="flex w-1/3 rounded-full bg-white p-4 items-center justify-center">Learn More</div>
+          </div>
         </div>
-      </main>
+      </div>
 
       <footer className={styles.footer}>
         <a
