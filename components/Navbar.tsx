1b323cc3e7f63ed6272a6529d688608df7bf611c
sanil011 <sanilp191@gmail.com>
2022-11-15 01:53:54 +0530

    changes in css of landing page , and track page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,8 +5,8 @@ import React from 'react'
 const Navbar = () => {
     const menu = [{ label: "About", route: "/" }, { label: "Timeline", route: "/timeline" }, { label: "Team", route: "/team" }, { label: "Tracks", route: "/tracks" }]
     return (
-        <div className="w-full h-fit text-black flex flex-row justify-start gap-10 items-center bg-transparent sticky top-0 z-[100] bg-gradient-to-r from-white via-transparent">
-            <img src="./AAP-logo.png" height={80} width={80} alt="AAP-logo" className="pl-5 py-1" />
+        <div className="w-full h-fit text-black flex flex-row justify-start gap-12 items-center bg-transparent sticky top-0 z-[100] bg-gradient-to-r from-white via-transparent">
+            <img src="./logo.svg" height={80} width={100} alt="AAP-logo" className="pl-5 py-1" />
             {menu.map((item, index) => {
                 return (
                     <div key={index} className="text-xl font-semibold">
---
+++ pages/tracks.tsx
@@ -0,0 +1,29 @@
+
+
+const Tracks = () => {
+    return (
+      <div className="px-28 py-28">
+          <h1 className="text-5xl">Domains</h1>
+          <ul className="pt-8">
+              <li className="text-2xl tracking-wide border-2 border-[#005550] py-6 my-6 text-white bg-[#005550] rounded-full text-center">
+                  <h2>Web development</h2>
+                </li>
+                <li className="text-2xl tracking-wide border-2 border-[#005550] py-6 my-6 text-white bg-[#005550] rounded-full text-center">
+                  <h2>UI/UX</h2>
+                </li>
+                <li className="text-2xl tracking-wide border-2 border-[#005550] py-6 my-6 text-white bg-[#005550] rounded-full text-center">
+                  <h2>Blockchain / Web3</h2>
+                </li>
+                <li className="text-2xl tracking-wide border-2 border-[#005550] py-6 my-6 text-white bg-[#005550] rounded-full text-center">
+                  <h2>Artificial Intelligence/Machine Learning</h2>
+                </li>
+                <li className="text-2xl tracking-wide border-2 border-[#005550] py-6 my-6 text-white bg-[#005550] rounded-full text-center">
+                  <h2>Android Development</h2>
+                </li>
+                
+          </ul>
+      </div>
+  )
+}
+
+export default Tracks
