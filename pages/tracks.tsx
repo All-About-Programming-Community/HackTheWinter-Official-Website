aba0c5fd50d43270d30804b229bfcc3e29aa7c00
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-11-15 17:02:26 +0530

    Update tracks.tsx in DRY fmt

--- pages/tracks.tsx
+++ pages/tracks.tsx
@@ -1,28 +1,19 @@
 
 
 const Tracks = () => {
-    return (
-      <div className="px-28 py-28">
-          <h1 className="text-5xl">Domains</h1>
-          <ul className="pt-8">
-              <li className="text-2xl tracking-wide border-2 border-[#005550] py-6 my-6 text-white bg-[#005550] rounded-full text-center">
-                  <h2>Web development</h2>
-                </li>
-                <li className="text-2xl tracking-wide border-2 border-[#005550] py-6 my-6 text-white bg-[#005550] rounded-full text-center">
-                  <h2>UI/UX</h2>
-                </li>
-                <li className="text-2xl tracking-wide border-2 border-[#005550] py-6 my-6 text-white bg-[#005550] rounded-full text-center">
-                  <h2>Blockchain / Web3</h2>
-                </li>
-                <li className="text-2xl tracking-wide border-2 border-[#005550] py-6 my-6 text-white bg-[#005550] rounded-full text-center">
-                  <h2>Artificial Intelligence/Machine Learning</h2>
-                </li>
-                <li className="text-2xl tracking-wide border-2 border-[#005550] py-6 my-6 text-white bg-[#005550] rounded-full text-center">
-                  <h2>Android Development</h2>
-                </li>
-                
-          </ul>
-      </div>
+  let tracks = ["Web Development", "UI/UX", "Blockchain/Web3.0", "Artificial Intelligence/ Machine Learning", "Android Development"]
+  return (
+    <div className="px-28 py-28">
+      <h1 className="text-5xl">Domains</h1>
+      <ul className="pt-8">
+        {tracks.map((track, idx) => {
+          return (
+          <li key={idx} className="text-2xl tracking-wide border-2 border-[#005550] py-6 my-6 text-white bg-[#005550] rounded-full text-center">
+            <h2>{track}</h2>
+          </li>
+          )})}
+      </ul>
+    </div>
   )
 }
 
